c2becf965a0be0251220b0a19c99cfdfce222824
unbrain <marsorsun@gmail.com>
2018-04-22 16:25:48 +0800

    增加markdown

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-let code = document.querySelector('#code')
-let n = 0
-let timer = setInterval(() => {
-  code.innerHTML = Prism.highlight(css1.substring(0, n), Prism.languages.css, 'css');
-  styleTag.innerHTML = css1.substring(0, n)
-  let now = css1.substring(n + 1, n)
-  if (now === '\n') {
-    console.log('enter')
-  } else {
+function writeCode(precode, code, callback) {
+  let pcode = precode || ''
+  let Domcode = document.querySelector('#code')
+  let n = 0
+  let timer = setInterval(() => {
+    Domcode.innerHTML = Prism.highlight(pcode + code.substring(0, n), Prism.languages.css, 'css');
+    styleTag.innerHTML = result = pcode + code.substring(0, n)
+    let now = code.substring(n + 1, n)
     pressKey(now);
-  }
-  code.scrollTop = code.scrollHeight
-  if (n === css1.length) {
-    window.clearInterval(timer)
-  }
-  n++
-}, 30)
+    Domcode.scrollTop = Domcode.scrollHeight
+    if (n === code.length) {
+      window.clearInterval(timer)
+      callback && callback.call()
+    }
+    n++
+  }, 30)
+}
+
 
 var css1 = `/* 
  * 面试官你好，我是朝阳
@@ -35,7 +36,7 @@ html{
   rgb(50, 255, 255);
 }
 #code{
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(0, 255, 0, 0.1);
   border: 1px solid #aaa;
   padding: 16px;
   transform: perspective(2500px) rotateX(-20deg);
@@ -61,7 +62,7 @@ html{
 
 /* 加一个呼吸效果 */
 #code{
-  animation: breath 0.5s infinite alternate-reverse;
+  animation: breath 1.5s infinite alternate-reverse;
 }
 @keyframes breath{
   0%{
@@ -74,21 +75,107 @@ html{
 
 /* 我需要一张白纸 */
 
-#code-wrapper{
-  width:100%; left: 0; position: fixed; 
-}
-/* 于是我就可以在白纸上写字了，请看右边 */
 `
 
 function pressKey(char) {
-  var key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
-  if (!key) {
-    document.querySelector('[data-char="hans"]').setAttribute('data-pressed', 'on');
-    return console.warn('No key for', char);
+  if (char === '\n') {
+    console.log('enter')
+  } else {
+    var key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
+    if (!key) {
+      document.querySelector('[data-char="hans"]').setAttribute('data-pressed', 'on');
+      return console.warn('No key for', char);
+    }
+    key.setAttribute('data-pressed', 'on');
+    setTimeout(function () {
+      document.querySelector('[data-char="hans"]').removeAttribute('data-pressed', 'on');
+      key.removeAttribute('data-pressed');
+    }, 0);
   }
-  key.setAttribute('data-pressed', 'on');
-  setTimeout(function () {
-    document.querySelector('[data-char="hans"]').removeAttribute('data-pressed', 'on');
-    key.removeAttribute('data-pressed');
-  }, 0);
 }
+
+function creatPaper(fn) {
+  var p = document.createElement('div')
+  p.id = 'paper'
+  document.querySelector('.interface').appendChild(p)
+  var content = document.createElement('pre')
+  content.className = 'content'
+  paper.appendChild(content)
+  fn()
+}
+
+function writeMarkdown(code, fn){
+  let Domcode = document.querySelector('#paper .content')
+  let n = 0
+  let timer = setInterval(() => {
+    Domcode.innerHTML = Prism.highlight(code.substring(0, n), Prism.languages.markdown, 'markdown');
+    let now = code.substring(n + 1, n)
+    pressKey(now);
+    Domcode.scrollTop = Domcode.scrollHeight
+    if (n === code.length) {
+      window.clearInterval(timer)
+      fn && fn()
+      
+    }
+    n++
+  }, 0)
+  
+}
+
+var css2 = `#paper {
+  width: 45%;
+  margin: .5em;
+  height: 90vh;
+  position: fixed;
+  display: flex;
+  align-items: flex-start;
+  top:0;
+  right: 0;
+  background: rgba(0, 0, 255, 0.1);
+  box-shadow: 0 0 40px rgba(0,0,0,0.5)
+}
+.content{
+  margin: 1em;
+  padding: 0em 5em;
+}`
+
+var css3 = `
+
+/* 使用 markedjs
+ * 这样就有样式了
+ */
+`
+
+var md = `
+# 自我介绍
+我叫 朝阳
+1996 年 
+成都工业学院 计算机科学与技术
+自学前端一年
+应聘前端开发岗位
+# 技能介绍
+熟悉 JavaScript CSS
+# 项目介绍
+1. XXX 轮播
+2. XXX 简历
+3. XXX 画板
+# 联系方式
+  - QQ 641959402
+  - Email marsorsun@gmail.com
+  - 手机 
+
+`
+
+writeCode('', css1, () => {
+  creatPaper(() => {
+    writeCode(css1, css2, () => {
+      writeMarkdown(md, () => {
+        writeCode(css1+css2, css3, () => {
+          document.querySelector('#paper .content').innerHTML = marked(md);
+          console.log('over')
+        })
+      })
+      
+    })
+  })
+})
