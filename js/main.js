30b726cb72962e12b463a10e7fffb0c23fd96adb
unbrain <marsorsun@gmail.com>
2018-04-22 13:17:13 +0800

    增加键盘

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
 let code = document.querySelector('#code')
 let n = 0
 let timer = setInterval(() => {
-    code.innerHTML = Prism.highlight(css1.substring(0, n), Prism.languages.css, 'css');
-    styleTag.innerHTML = css1.substring(0, n)
-    code.scrollTop = code.scrollHeight
-    if(n === css1.length){
-        window.clearInterval(timer)
-    }
-    n++
-}, 0)
+  code.innerHTML = Prism.highlight(css1.substring(0, n), Prism.languages.css, 'css');
+  styleTag.innerHTML = css1.substring(0, n)
+  let now = css1.substring(n + 1, n)
+  if (now === '\n') {
+    console.log('enter')
+  } else {
+    pressKey(now);
+  }
+  code.scrollTop = code.scrollHeight
+  if (n === css1.length) {
+    window.clearInterval(timer)
+  }
+  n++
+}, 30)
 
 var css1 = `/* 
  * 面试官你好，我是朝阳
- * 只用文字作做我介绍太单调了
- * 我就用代码来介绍吧
- * 首先准备一些样式
+ * 咦～ 我的键盘呢？
  */
+
 *{
   transition: all 1s;
-}
-html{
-  background: #33485E;
   
 }
+.keyboard {
+    display: block;
+    transform: perspective(1000px) rotateX(30deg);
+    background: rgba(255, 25, 25, 0.1);
+}
+
+html{
+  rgb(50, 255, 255);
+}
 #code{
-  background: rgba(255, 255, 255, 0.2);
+  background: rgba(0, 0, 0, 0.1);
   border: 1px solid #aaa;
   padding: 16px;
-  transform: perspective(2000px) rotateY(20deg);
+  transform: perspective(2500px) rotateX(-20deg);
+  height: 75vh;
 }
 
 /* 使用Prsim
@@ -53,10 +65,10 @@ html{
 }
 @keyframes breath{
   0%{
-    box-shadow: 0 0 20px rgba(0,0,0,1)
+    box-shadow: 0 0 40px rgba(0,0,0,1)
   }
   100%{
-    box-shadow: 0 0 20px rgba(0,0,0,0.5)
+    box-shadow: 0 0 40px rgba(0,0,0,0.5)
   }
 }
 
@@ -67,3 +79,16 @@ html{
 }
 /* 于是我就可以在白纸上写字了，请看右边 */
 `
+
+function pressKey(char) {
+  var key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
+  if (!key) {
+    document.querySelector('[data-char="hans"]').setAttribute('data-pressed', 'on');
+    return console.warn('No key for', char);
+  }
+  key.setAttribute('data-pressed', 'on');
+  setTimeout(function () {
+    document.querySelector('[data-char="hans"]').removeAttribute('data-pressed', 'on');
+    key.removeAttribute('data-pressed');
+  }, 0);
+}
